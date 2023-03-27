c86ed70e78642b089793b8c13b179f747793a4da
CodingApe9 <pos06098@gmail.com>
2023-03-27 19:28:12 +0900

    "update calc"

--- kotlin/calc.kt
+++ kotlin/calc.kt
@@ -1,29 +1,31 @@
 fun main(args: Array<String>) {
-    val exp = "5 + 4"
+//    val exp = "5 + 4"
 //    val exp = "3 / 0"
 //    val exp = "1 # 3"
 //    val exp = "1 2 3 4"
 //    val exp = "# 2 *"
 //    val exp = null
-//    val exp = ""
+    val exp = ""
 //    val exp = readLine();
     val splitedExp = split(exp)
     println("spitedExp = ${splitedExp}")
 
+    val checkSizeAndOperand:((List<String>) -> Boolean) = {splitedExp ->
+        splitedExp.size == 3
+            && Character.isDigit(splitedExp[0].toCharArray()[0])
+            && Character.isDigit(splitedExp[2].toCharArray()[0])}
+
     //계산기 정책
     val p1:((List<String>) -> Boolean) = {splitedExp ->
-        splitedExp.size == 3
-                && Character.isDigit(splitedExp[0].toCharArray()[0])
-                && Character.isDigit(splitedExp[2].toCharArray()[0])
+        checkSizeAndOperand(splitedExp)
                 && ((splitedExp[1]=="+" || splitedExp[1]=="-" || splitedExp[1]=="*")
-                    || (splitedExp[1]=="/"
-                    && (splitedExp[0]!="0" && splitedExp[2]!="0")))}
+                    || (splitedExp[1]=="/" && (splitedExp[0]!="0" && splitedExp[2]!="0")))}
 
     val p2:((List<String>) -> Boolean) = {spitedExp ->
-        p1(spitedExp)
-                || (spitedExp[1]=="%" && spitedExp[0]!="0" && spitedExp[2]!="0")}
+        checkSizeAndOperand(splitedExp)
+                && (p1(spitedExp) || (spitedExp[1]=="%" && spitedExp[0]!="0" && spitedExp[2]!="0"))}
 
-    val func = parse(splitedExp, p1)
+    val func = parse(splitedExp, p2)
     println("func = ${func}")
 
     val result = func?.let { it(splitedExp[0]!!.toDouble(), splitedExp[2]!!.toDouble()) } ?: "can't calculate"
@@ -31,7 +33,7 @@ fun main(args: Array<String>) {
 }
 
 //스트링을 공백 기준으로 스플릿한 List<String>로 리턴, 입력 스트링이 null이면 빈 문자열이 들어간 리스트로 리턴
-val split:((String?) ->  List<String>)= { exp -> exp?.split(" ")?: listOf("") }
+val split:(String?) ->  List<String> = { exp -> exp?.split(" ")?: listOf("") }
 
 //정책에 맞는지 확인하고 아니라면 null을 리턴
 //만약 정책에 맞는 입력 즉 계산이 가능한 입력이라면 그에 맞는 함수((Double, Double) -> Double)를 리턴
